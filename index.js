4e30a72c4c60af2e2be8b83193605f967fada670
neuronet.io <neuronet.io@gmail.com>
2020-08-09 20:30:17 +0200

    update

--- index.js
+++ index.js
@@ -2,98 +2,104 @@ const NodeRSA = require('node-rsa');
 const CryptoJS = require('crypto-js');
 const fs = require('fs');
 
-class SoftwareLicenseKey {
-
-  constructor(cert=null) {
+class LicenseSerial {
+  constructor(cert = null) {
     if (cert == null) {
-      this.key = new NodeRSA();
-      this.key.generateKeyPair();
+      this.rsa = new NodeRSA();
+      this.rsa.generateKeyPair();
     } else {
-      this.key = new NodeRSA(cert);
+      this.rsa = new NodeRSA(cert);
     }
   }
 
   generateLicense(licenseKeyData) {
-
-    if (!this.key.isPrivate()) {
-      throw "Cannot generate license key. Key provided is not private.";
-      return;
+    if (!this.rsa.isPrivate()) {
+      throw 'Cannot generate license key. Key provided is not private.';
     }
 
     if (licenseKeyData == null) {
-      throw "No license key data provided.";
-      return;
+      throw 'No license key data provided.';
     }
 
-    var randomSymmetricKey = CryptoJS.lib.WordArray.random(128/8).toString(CryptoJS.enc.Base64);
-    var encrypteddata = CryptoJS.AES.encrypt(JSON.stringify(licenseKeyData), randomSymmetricKey).toString();
+    const randomSymmetricKey = CryptoJS.lib.WordArray.random(128 / 8).toString(
+      CryptoJS.enc.Base64
+    );
+    const encryptedData = CryptoJS.AES.encrypt(
+      JSON.stringify(licenseKeyData),
+      randomSymmetricKey
+    ).toString();
 
     //encrypt the symmetric key.
-    var encryptedSymmetricKey = this.key.encryptPrivate(randomSymmetricKey, 'base64');
+    const encryptedSymmetricKey = this.rsa.encryptPrivate(
+      randomSymmetricKey,
+      'base64'
+    );
 
     //combine the encrypted symmetric key with the encrypted data
-    var msg = encryptedSymmetricKey + "||" + encrypteddata;
+    const encrypted = encryptedSymmetricKey + '||' + encryptedData;
 
     // build the signature for the license key data
-    var signature = this.key.sign(JSON.stringify(licenseKeyData), 'base64');
+    const signature = this.rsa.sign(JSON.stringify(licenseKeyData), 'base64');
 
-    //build license Key
-    var licenseKey = "====BEGIN LICENSE KEY====\n";
-    licenseKey += msg + "\n";
+    let licenseKey = '====BEGIN LICENSE KEY====\n';
+    licenseKey += encrypted + '\n';
     licenseKey += signature;
-    licenseKey += "\n====END LICENSE KEY====";
+    licenseKey += '\n====END LICENSE KEY====';
 
     return licenseKey;
   }
 
   validateLicense(licenseKey) {
     //parse the license key
-    var lines = licenseKey.split("\n");
-    var keyMsg = lines[1].split("||");
-    var signature = lines[2];
+    licenseKey = licenseKey.trim();
+    //parse the license key
+    const lines = licenseKey.split('\n');
+    const keyMsg = lines[1].split('||');
+    const signature = lines[2];
+
+    let randomSymmetricKey = '',
+      decryptedData = '';
 
     //decrypt the random symmetric key
     try {
-      var randomSymmetricKey = this.key.decryptPublic(keyMsg[0], 'utf8');
-    } catch(e) {
-      throw "Invalid data: Could not extract symmetric key.";
-      return;
+      randomSymmetricKey = this.rsa.decryptPublic(keyMsg[0], 'utf8');
+    } catch (e) {
+      throw 'Invalid data: Could not extract symmetric key.';
     }
 
     //decrypt the payload
     try {
-      var decrypteddata = CryptoJS.AES.decrypt(keyMsg[1], randomSymmetricKey).toString(CryptoJS.enc.Utf8);
+      decryptedData = CryptoJS.AES.decrypt(
+        keyMsg[1],
+        randomSymmetricKey
+      ).toString(CryptoJS.enc.Utf8);
     } catch (e) {
-      throw "Invalid Data: Could not decrypt data with key found.";
-      return;
+      throw 'Invalid Data: Could not decrypt data with key found.';
     }
 
-
     //verify the signature.
-    if (this.key.verify(decrypteddata, signature, 'utf8', 'base64')) {
-      //return the decryped data.
-      return JSON.parse(decrypteddata);
+    // @ts-ignore
+    if (this.rsa.verify(decryptedData, signature, 'utf8', 'base64')) {
+      //return the decrypted data.
+      return JSON.parse(decryptedData);
     } else {
-      throw "License Key signature invalid. This license key may have been tampered with";
-      return;
+      throw 'License Key signature invalid. This license key may have been tampered with';
     }
   }
 
   /**
    * Exports the private key in PEM format
-  */
+   */
   exportPrivateKey(filePath) {
-
-    if (!this.key.isPrivate()) {
-      throw "The key is not a private key. Cannot export private key from public key."
-      return;
+    if (!this.rsa.isPrivate()) {
+      throw 'The key is not a private key. Cannot export private key from public key.';
     }
     if (filePath == null) {
-      return this.key.exportKey();
+      return this.rsa.exportKey();
     } else {
       try {
-        fs.writeFileSync(filePath, this.key.exportKey());
-      } catch(e) {
+        fs.writeFileSync(filePath, this.rsa.exportKey());
+      } catch (e) {
         throw e; //error writing the private key out to disk.
       }
     }
@@ -101,18 +107,18 @@ class SoftwareLicenseKey {
 
   /**
    * Exports the public key
-  */
+   */
   exportPublicKey(filePath) {
     if (filePath == null) {
-      return this.key.exportKey('public');
+      return this.rsa.exportKey('public');
     } else {
       try {
-        fs.writeFileSync(filePath, this.key.exportKey('public'));
-      } catch(e) {
+        fs.writeFileSync(filePath, this.rsa.exportKey('public'));
+      } catch (e) {
         throw e; //error writing the private key out to disk.
       }
     }
   }
 }
 
-module.exports = SoftwareLicenseKey;
+module.exports = LicenseSerial;
